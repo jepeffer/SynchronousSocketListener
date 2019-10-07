3c1da19215d666819c923f2746c4cd1a0847702f
Julian Peffer <jepeffer@unomaha.edu>
2019-10-07 18:41:52 -0500

    Init

---
+++ SynchronousSocketListener/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SynchronousSocketListener
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new SyncServer();
+        }
+    }
+}
